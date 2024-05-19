a50f031eabf09b463fce0194eff7b59cf5f93131
Micael Viana <micael.vianasilva@gmail.com>
2024-05-18 21:25:24 -0400

    update comment.nvim

--- after/plugin/comment-nvim.lua
+++ after/plugin/comment-nvim.lua
@@ -1,7 +1,7 @@
 require('Comment').setup()
 local ft = require('Comment.ft')
 
---attacking the antlr problem
+--tackling the antlr problem
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
   pattern = "*.g",
   command = "set filetype=antlr"
